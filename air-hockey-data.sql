46851fd5ad66009b0e66e98e3eb77467df5dc95d
Arnaud BODEL <contact@osw3.net>
2018-11-30 12:21:26 +0100

    -

---
+++ air-hockey-data.sql
@@ -0,0 +1,18 @@
+INSERT INTO `countries` (`name`, `alpha2`) VALUES ('France', 'FR');
+INSERT INTO `countries` (`name`, `alpha2`) VALUES ('Germany', 'DE');
+INSERT INTO `countries` (`name`, `alpha2`) VALUES ('Spain', 'SP');
+INSERT INTO `countries` (`name`, `alpha2`) VALUES ('Netherland', 'NL');
+
+INSERT INTO `players` (`name`, `country`) VALUES ('Jean', 'FR');
+INSERT INTO `players` (`name`, `country`) VALUES ('Pierre', 'FR');
+INSERT INTO `players` (`name`, `country`) VALUES ('Franz', 'DE');
+INSERT INTO `players` (`name`, `country`) VALUES ('Jurgen', 'DE');
+INSERT INTO `players` (`name`, `country`) VALUES ('Carlos', 'space()');
+INSERT INTO `players` (`name`, `country`) VALUES ('Juan', 'SP');
+INSERT INTO `players` (`name`, `country`) VALUES ('Rijkaard', 'NL');
+INSERT INTO `players` (`name`, `country`) VALUES ('Jan', 'NL');
+
+INSERT INTO `teams` (`name`, `country`) VALUES ('Equipe de France', 'FR');
+INSERT INTO `teams` (`name`, `country`) VALUES ('die Wurstmannschaft', 'DE');
+INSERT INTO `teams` (`name`, `country`) VALUES ('team Ibiza y playa', 'SP');
+INSERT INTO `teams` (`name`, `country`) VALUES ('De WeedCrew', 'NL');
---
+++ air-hockey-structure.sql
@@ -0,0 +1,107 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: air_hockey
+-- ------------------------------------------------------
+-- Server version	5.7.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `countries` (
+  `id` tinyint(2) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `alpha2` char(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `matches`
+--
+
+DROP TABLE IF EXISTS `matches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matches` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
+  `team_a` int(5) NOT NULL,
+  `team_b` int(5) NOT NULL,
+  `score_a` tinyint(3) NOT NULL DEFAULT '0',
+  `score_b` tinyint(3) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `player_teams`
+--
+
+DROP TABLE IF EXISTS `player_teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player_teams` (
+  `id_player` int(5) NOT NULL,
+  `id_team` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `players` (
+  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `country` char(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teams` (
+  `id` int(5) unsigned NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `country` char(2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-30 11:44:49
